2550c32df345d1009077276cd03d8a07789eb121
Thomas Harvey <thomasharvey24@gmail.com>
2015-03-15 19:57:44 -0400

    Finished tested BattleShip Class

--- project/models.py
+++ project/models.py
@@ -1,121 +1,245 @@
-# import sqlite3
-
+# models redux
 class Player:
     def __init__(self, name):
         self.name = name
         self.board = Board()
+        # need something to add ships or the 
+        # ship needs to be added in the game
+        self.fleet = []
+        # below applies to the player not the players board
+        self.previous_targets = []
+        # should the player keep track of the previous
+        # guesses
+        # ship_sizes = {'Aircraft Carrier':5,'BattleShip':4,'Submarine':3,'Destroyer':3,'Patrol Boat':2}
 
 class Ship:
-    # ship_sizes = {'Aircraft Carrier':5,'BattleShip':4,'Submarine':3,'Destroyer':3,'Patrol Boat':2}
     def __init__(self, name, size):
         self.name = name
         self.size = size
-        # might remove this with dictionary
-        self.location = []
-        self.hits = []
-
-    def add_coordinates(self, coordinates_tuples):
-        self.location = coordinates_tuples
-        # change is hit to hit and have it mark it self as hit
-    def is_hit(self, coordinate):
-        if coordinate in self.location and coordinate not in self.hits:
-            self.hits.append(coordinate)
-            return True
-        return False
-
+        self.hits = 0
+    def hit(self):
+        self.hits += 1
     def is_sunk(self):
-        return len(self.hits) == len(self.location)
+        return self.hits == self.size
 
 class Board:
     def __init__(self, size=10):
         self.size = size
-        # new idea below
-        # self.board = {}
-        self.ships_on_board = []
-        self.previous_targets = []
-        self.display = [['  ~  'for i in range(size)] for j in range(size)]
-        # self.markers = {'hit':'*', 'miss':chr(164), 'wave':'~'
-    def add_ship_location(self, ship):
-        self.ships_on_board.append(ship)
+        self.locations = {}
+    # creates key and checks to see if it is valid
+    # i like this below
+    # tested
+    def create_board(self):
+        self.locations = {chr(65+j)+str(i+1): '~' for i in range(self.size) for j in range(self.size)}
+
+    def board_key(self, string_value):
+        column = "".join([char.upper() for char in string_value if char.isalpha()])
+        if ord(column) > 64 + self.size or ord(column) < 65:
+            return False  
+        row = "".join([number for number in string_value if number.isdigit()])
+        if int(row) > self.size or int(row) < 1:
+            return False
+        dict_key_format = column + row
+        return dict_key_format
+    # returns the value at the key provided if the key is in the dict 
+    # i dont like this it shouldnt return the ship value the ship value 
+    # contains info about the ship
+    # tested
+    # using to place ships
+    def value_at_location(self, string_key):
+        dict_key = self.board_key(string_key)
+        if dict_key:
+            return self.locations[dict_key]
+        return False
+    # i like this below
+    # sets key value to the value provided if the key is in the dict
+    def edit_board(self, string_value, value):
+        key = self.board_key(string_value)
+        if key:
+            self.locations[key] = value
+            return True
+        return False
+    # add dict value to location here 
+    def display_board(self):
+        display = []
+        for j in range(self.size):
+            line = []
+            for i in range(self.size):
+                if isinstance(self.locations[chr(65+i)+str(j+1)], str):
+                    line.append('  {}  '.format(self.locations[chr(65+i)+str(j+1)]))
+                else:
+                    line.append('  ~  ')
+                    
+            display.append(line)
+        return display
 
 
+
+# any interaction between a player and another players board occurs in this class
 class BattleShip:
+    # adjusted for new board model
     def __init__(self):
         self.players = []
-        self.boards = []
-        self.ship_sizes = {'Aircraft Carrier':5,'BattleShip':4,'Submarine':3,'Destroyer':3,'Patrol Boat':2}
+        self.war_time = False
+        # self.ship_sizes = {'Aircraft Carrier':5,'BattleShip':4,'Submarine':3,'Destroyer':3,'Patrol Boat':2}
     # called in controller
-    def add_player(self, player, board):
+    # adjusted for new board model
+    # TESTED
+    def add_player(self, player):
         self.players.append(player)
-        self.boards.append(board)
-    # no input
     # called in controller
+    # adjusted for new board model
+    # TESTED
     def end_turn(self):
-        if self.game_over():
+        if self.game_over() and self.war_time:
             return False
         last_player = self.players.pop(0)
         self.players.append(last_player) 
-
-        last_board = self.boards.pop(0)
-        self.boards.append(last_board)
         return True
-    # takes two coordinates inputs from user
-    # called in controller
-    def place_ship(self, start_coor, end_coor, ship):
-        place_ship_here = []
-        if start_coor[0] == end_coor[0]:
-            if abs(start_coor[1]-end_coor[1]) == ship.size:
-                
-                y_coor = self.coordinate_generator(start_coor[1],end_coor[1])
-                place_ship_here = [(start_coor[0],y) for y in y_coor]
-
-        elif start_coor[1] == end_coor[1]:
-            if abs(start_coor[0]-end_coor[0]) == ship.size:
-
-                x_coor = self.coordinate_generator(start_coor[0],end_coor[0])
-                place_ship_here = [(x,start_coor[1]) for x in x_coor]
-
-        if place_ship_here and not self.occupied(place_ship_here):
-            ship.location = place_ship_here
-            self.boards[0].add_ship_location(ship)
-            return True
-        return False
-
-    def coordinate_generator(start, end):
-        step = int(start-end / -(start-end))
-        return [i for i in range(start, end+step, step)]
-    # input from BattleShip.place_ship()
-    # called in BattleShip.place_ship()
-    def occupied(self, coordinates):
-        for ship.location in self.boards[0].ships_on_board:
-            for coordinate_tuple in coordinates:
-                if coordinate_tuple in ship.location:
-                    return True
-        return False
-    # user inputs coordinates
-    # called in the controller
-    def shoot_at(self, coordinate):
-        if coordinate in self.boards[1].previous_targets:
-            return False
-        self.boards[1].previous_targets.append(coordinate)
-        return True
-    
-    # no input
-    # call in controller
-    def check_ships(self):
-        for ship in self.boards[1].ships_on_board:
-            if not ship.is_sunk() and ship.is_hit(self.boards[1].previous_targets[-1]):
-                coordinate = self.boards[1].previous_targets[-1]
-                self.boards[1].display[coordinate[1]][coordinate[0]] = '*'
-                return True
-        self.boards[1].display[coordinate[1]][coordinate[0]] = chr(164)
-        return False
-    
+    # edit name of this and the instance variable
+    # TESTED
+    def current_board(self):
+        if self.war_time:
+            target = 1
+        else:
+            target = 0
+        return self.players[target].board
     # no input
     # called in BattleShip.game_over()
+    # adjusted for new board model
+    # Half Tested
     def game_over(self):
-        for ship in self.boards[1].ships_on_board:
+        for ship in self.players[1].fleet:
             if not ship.is_sunk():
                 return False
         return True
 
+    # adjusted I Think not sure
+    # slim this down 
+     # TESTED
+    def ship_location_validator(self, start_coor, end_coor, ship):
+        # should probably pass a player into this method 
+        target_board = self.current_board()
+
+        start = target_board.board_key(start_coor)
+        end = target_board.board_key(end_coor)
+
+
+        # calling the board_key method ensures a letter will be
+        # at index 0
+        if not start or not end:
+            return False
+
+        # should the board be responsible for calculating the 
+        # distances? 
+        # I've decided the board should handle this
+        # instead of the game.
+        # check all sides using direction_of_ship
+        fixed = self.direction_of_ship(start,end)
+        if not fixed:
+            return False
+        elif abs(int(start[1:])-int(end[1:]))+1 == ship.size:
+           
+            row_values = self.coordinate_generator(int(start[1:]),int(end[1:]))
+            valid_location = [target_board.board_key(fixed + str(i)) for i in row_values]
+            return valid_location
+
+        elif abs(ord(start[0])-ord(end[0]))+1 == ship.size:
+
+            column_values = self.coordinate_generator(ord(start[0]),ord(end[0]))
+            valid_location = [target_board.board_key(fixed + chr(x)) for x in column_values]
+            return valid_location
+
+        else:
+            return False
+    # might move this to the board
+    # although it does not use any board methods
+    # UPDATE Must Move this to Board
+    # UPDATE The other methods i intend to move need this method
+    # Rename this in board class
+    # TESTED
+    def direction_of_ship(self, start, end):
+        if start[0] == end[0]:
+            return start[0]
+        elif start[1:] == end[1:]:
+            return start[1:]
+        else:
+            return False
+    # this should be moved to the board class
+    # UPDATE Must Move this to Board
+    # TESTED
+    def coordinate_generator(self, start, end):
+        step = 1 - 2 * (start > end)
+        return [i for i in range(start, end+step, step)]
+
+    # has not been optimized for ai
+    # TESTED
+    def place_ship_here(self, locations, ship):
+        if self.occupied(locations):
+            return False
+        for key in locations:
+            # might not want to use current_board() here
+            self.current_board().edit_board(key, ship)   
+        self.players[0].fleet.append(ship) 
+        return True
+
+    # input from BattleShip.ship_location_validator()
+    # called in BattleShip.ship_location_validator()
+    # has not been optimized for ai
+    # TESTED
+    def occupied(self, coordinates):
+        # has to be adapted
+        current_board = self.players[0].board
+        for key in coordinates:
+            # USES VALUE_AT
+            if current_board.value_at_location(key) != '~':
+                return True
+        return False
+    # user inputs coordinates
+    # called in the controller
+    # has not been optimized for ai
+    def shoot_at(self, coordinate):
+        target_board = self.current_board()
+        target_key = target_board.board_key(coordinate)
+
+        if not target_key or target_key in self.players[0].previous_targets:
+            return False
+        # USES VALUE_AT
+        if target_board.value_at_location(target_key) != '~':
+            target_board.value_at_location(target_key).hit()
+            # target_board.value_at_location(target_key).is_sunk()
+            # Value to insert at target location
+            value = "*"
+            
+        else:
+            # Value to insert at target location
+            value = chr(164)
+
+        target_board.edit_board(target_key, value)
+        self.players[0].previous_targets.append(target_key)
+        return True
+
+
+
+
+def set_up_game():
+    ac = Ship("Aircraft Carrier", 5)
+    destroyer = Ship("Destroyer", 3)
+
+    game = BattleShip()
+    game.add_player(Player("Zack"))
+    game.add_player(Player("Bill"))
+    game.players[0].board.create_board()
+    game.players[1].board.create_board()
+    ship_local = game.ship_location_validator("9a","9c",destroyer)
+    game.place_ship_here(ship_local, destroyer)
+    ship_local = game.ship_location_validator("2e","e6",ac)
+    game.place_ship_here(ship_local, ac)
+    # [print(line) for line in game.players[0].board.display_board()]
+    return game
+# game.ship_location_validator("F6","9f",destroyer)
+# game = set_up_game()
+# game.current_board().display_board()
+# game.shoot_at("1a")
+# print(game.current_board().display_board())
+# [print(line) for line in test.current_board().display_board()] 
