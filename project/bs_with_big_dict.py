35cd03ccc2fd09184894b49612d9c20fed11ec89
Thomas Harvey <thomasharvey24@gmail.com>
2015-03-15 11:42:51 -0400

    Adjusting Board class and bs game

--- project/bs_with_big_dict.py
+++ project/bs_with_big_dict.py
@@ -73,7 +73,6 @@ class BattleShip:
     # adjusted for new board model
     def add_player(self, player):
         self.players.append(player)
-    # no input
     # called in controller
     # adjusted for new board model
     def end_turn(self):
@@ -82,42 +81,82 @@ class BattleShip:
         last_player = self.players.pop(0)
         self.players.append(last_player) 
         return True
-    # takes two coordinates inputs from user
-    # called in controller
+    # no input
+    # called in BattleShip.game_over()
+    # adjusted for new board model
+    def game_over(self):
+        for ship in self.players[1].fleet:
+            if not ship.is_sunk():
+                return False
+        return True
+    
+    # Slim ship_location_validator
+
     # adjusted I Think not sure
-    def place_ship(self, start_coor, end_coor, ship):
-        place_ship_here = []
+    # should specify the board in question for this method
+    # slim this down 
+    def ship_location_validator(self, start_coor, end_coor, ship):
+        valid_location = []
+        # should probably pass a player into this method 
         current_board = self.players[0].board
 
         start = current_board.board_key(start_coor)
         end = current_board.board_key(end_coor)
         # calling the board_key method ensures a letter will be
         # at index 0
+        if not start or not end:
+            return False
+        # should the board be responsible for calculating the 
+        # distances? 
+        # I've decided the board should handle this
+        # instead of the game.
+        fixed = direction_of_ship(start,end)
+
+        if not fixed:
+            return False
+        elif abs(int(start[1:])-int(end[1:])) == ship.size:
+            
+            row_values = self.coordinate_generator(int(start[1:]),int(end[1:]))
+            valid_location = [current_board.board_key(fixed + str(i)) for i in row_values]
+            return valid_location
+
+        elif abs(ord(start[0])-ord(end[0])) == ship.size:
+
+            column_values = self.coordinate_generator(ord(start[0]),ord(end[0]))
+            valid_location = [current_board.board_key(fixed + chr(x)) for x in column_values]
+            return valid_location
+
+        else:
+            return False
+    # might move this to the board
+    # although it does not use any board methods
+    # UPDATE Must Move this to Board
+    # UPDATE The other methods i intend to move need this method
+    # Rename this in board class
+    def direction_of_ship(start, end):
         if start[0] == end[0]:
-            if abs(int(start[1:])-int(end[1:])) == ship.size:
-                
-                row_values = self.coordinate_generator(int(start[1:]),int(end[1:]))
-                place_ship_here = [start[0]+str(i) for i in row_values]
-
+            return start[0]
         elif start[1:] == end[1:]:
-            if abs(ord(start[0])-ord(end[0])) == ship.size:
-
-                column_values = self.coordinate_generator(ord(start[0]),ord(end[0]))
-                place_ship_here = [chr(x) + start[1:] for x in column_values]
-
-        if place_ship_here and not self.occupied(place_ship_here):
-            for key in place_ship_here:
-                current_board.edit_board(key, ship)
-            return True
-        return False
-    # adjusted for new version
+            return start[1:]
+        else:
+            return False
+    # this should be moved to the board class
+    # UPDATE Must Move this to Board
     def coordinate_generator(start, end):
         step = int(start-end / -(start-end))
         return [i for i in range(start, end+step, step)]
 
-    # input from BattleShip.place_ship()
-    # called in BattleShip.place_ship()
-    # adjusted
+    # has not been optimized for ai
+    def place_ship_here(locations, ship):
+        if self.occupied(locations):
+            return False
+        for key in locations:
+            current_board.edit_board(key, ship)    
+        return True
+
+    # input from BattleShip.ship_location_validator()
+    # called in BattleShip.ship_location_validator()
+    # has not been optimized for ai
     def occupied(self, coordinates):
         current_board = self.players[0].board
         for key in coordinates:
@@ -127,7 +166,7 @@ class BattleShip:
         return False
     # user inputs coordinates
     # called in the controller
-    # adjusted 
+    # has not been optimized for ai
     def shoot_at(self, coordinate):
         target_board = self.players[1].board
         target_key = target_board.board_key(coordinate)
@@ -147,13 +186,4 @@ class BattleShip:
 
         target_board.edit_board(key, value)
         self.players[0].previous_targets.append(target_key)
-        return True
-
-    # no input
-    # called in BattleShip.game_over()
-    # adjusted for new board model
-    def game_over(self):
-        for ship in self.players[1].fleet:
-            if not ship.is_sunk():
-                return False
         return True
